b9f8f293261a27b17eafb9b5ca78dc1d9ac99959
fabiohq <fabio12hq@hotmail.com>
2019-11-13 18:04:12 -0500

    ***Tercer commit***

--- src/main/java/com/johnpantoja/HuevosCodornizApplication.java
+++ src/main/java/com/johnpantoja/HuevosCodornizApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HuevosCodornizApplication {
- //Metodo main Fabio
+ //Metodo main Fabio *****
 	public static void main(String[] args) {
 		SpringApplication.run(HuevosCodornizApplication.class, args);
 	}
